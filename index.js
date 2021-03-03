0e14b6f9774eda720e88876029ea21e458d7a783
darkplan20 <79349074+darkplan20@users.noreply.github.com>
2021-03-03 23:13:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help , ${client.guilds.cache.size} Server ,  {client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} `, { type: "LISTENING" });
+  client.user.setActivity(`${PREFIX}help and ${PREFIX}play, { type: "LISTENING" });
 
 });
 client.on("warn", (info) => console.log(info));
