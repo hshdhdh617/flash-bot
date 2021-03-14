741847a48c9c48498a0061ba44cf50277082a96f
Dragon <79349074+YT-Dragon@users.noreply.github.com>
2021-03-14 21:00:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} server | users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, { type: "PLAYING" });
+  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
 
 
 });
